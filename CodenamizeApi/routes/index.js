f570e0f3e2e986bb4dbe0a300f666563f30ce0fd
Stephen Anderson <teedor@gmail.com>
2023-07-20 15:33:35 +0100

    added codenamize

--- CodenamizeApi/routes/index.js
+++ CodenamizeApi/routes/index.js
@@ -1,10 +1,18 @@
 'use strict';
+var codenamize = require('@codenamize/codenamize');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    // Check if query parameter exists
+    if (req.query.value) {
+        // Respond with a JSON object containing the passed value
+        res.json({ codenamizedValue: codenamize(req.query.value) });
+    } else {
+        // If no query parameter, respond with the default page
+        res.status(400).json({ message: 'no value provided' });
+    }
 });
 
 module.exports = router;
