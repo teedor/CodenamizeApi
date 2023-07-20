431448e0af6f59731e09bdac2b19de45a146cfe9
Stephen Anderson <teedor@gmail.com>
2023-07-20 16:02:13 +0100

    added post to use multiple words

--- CodenamizeApi/routes/index.js
+++ CodenamizeApi/routes/index.js
@@ -4,6 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+// Middleware to handle JSON bodies 
+router.use(express.json());
+
+
 router.get('/', function (req, res) {
     // Check if query parameter exists
     if (req.query.value) {
@@ -12,6 +17,17 @@ router.get('/', function (req, res) {
     } else {
         // If no query parameter, respond with the default page
         res.status(400).json({ message: 'no value provided' });
+    }
+});
+
+/* POST home page. */
+router.post('/', function (req, res) {
+    if (req.body.values) {
+        // Map the input array to an array of codenamize results
+        var output = req.body.values.map(value => ({ original: value, codename: codenamize(value) }));
+        res.json(output);
+    } else {
+        res.status(400).send('Bad Request');
     }
 });
 
